ea7016bb6d02ab8b10f0fe85b778b105ce2cc6e1
Timur <t.fralik@aimprosoft.com>
2022-09-03 12:37:30 +0300

    Add implementation of Countable interface

--- src/Collection.php
+++ src/Collection.php
@@ -2,7 +2,24 @@
 
 namespace App;
 
-class Collection
-{
+use Countable;
 
+class Collection implements Countable
+{
+    private array $items;
+
+    public function __construct(array $items)
+    {
+        $this->items = $items;
+    }
+
+    public static function make(array $items): Collection
+    {
+        return new static($items);
+    }
+
+    public function count(): int
+    {
+        return count($this->items);
+    }
 }
--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -7,5 +7,20 @@ use PHPUnit\Framework\TestCase;
 
 class CollectionTest extends TestCase
 {
+    /** @test */
+    public function it_creates_collection()
+    {
+        $items = ['one', 'two', 'three'];
 
+        $this->assertInstanceOf(Collection::class, Collection::make($items));
+        $this->assertInstanceOf(Collection::class, new Collection($items));
+    }
+
+    /** @test */
+    public function it_wraps_an_array_of_items()
+    {
+        $items = ['one', 'two', 'three'];
+
+        $this->assertCount(3, Collection::make($items));
+    }
 }
